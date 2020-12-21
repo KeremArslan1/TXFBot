3e799cc059241d9762336b4c3fb14f93c3d94551
Kerem Arslan <75943060+KeremArslan1@users.noreply.github.com>
2020-12-21 22:45:50 +0300

    updated __init__.py
    This update is for Bot status. It sends a message to my discord server's

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,5 +1,8 @@
 from discord import Intents
 from discord import Embed
+
+from datetime import datetime
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
 
@@ -42,15 +45,34 @@ class Bot(BotBase):
             print("Bot hazır!")
 
             channel = self.get_channel(720319247628369950)
-            await channel.send("Bot hazır!")
+            await channel.send("Bot status:")
 
-            embed = Embed(title="Bot Aktif", description="The X Files botu artık aktif!")
+            embed = Embed(title="Bot Aktif", description="The X Files botu artık aktif!",
+                          colour=0x00FF00, timestamp=datetime.utcnow())
             fields = [("Yapımcı", "Kerem Arslan#0001", True),
                         ("Ana sunucu Link", "https://discord.gg/Mtjvspv", True),
                         ("Bot Hakkında", "Bu botun yapılma amacı The X Files dizisi hakkındaki Türkiye discord sunucusunu geliştirmektir.", False)]
             for name, value, inline in fields:
                 embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="The X Files Türkiye", icon_url="https://i.imgur.com/KLjU8fb.jpg")
+            embed.set_footer(text="Yakında komut eklemeyi başarabileceğim -umarım-")
+            embed.set_thumbnail(url=self.guild.icon_url)
+            
             await channel.send(embed=embed)
+
+            '''
+            if you want to add extra image in embed: embed.set_image(url="image url or location")
+            '''
+
+            '''
+            if you want to put photo from database, you need follow these steps:
+            change "From discord import Embed" to "From discord import Embed, File"
+            add a folder named "images" in data folder
+            put whatever photo you want
+            the path is = "./data/images/image_name.extansion"
+            I am going to use imgur because you are able to see the picture :) (and lot easier than local files)
+            '''
+
         else:
             print("Bot yeniden bağlandı!")
 

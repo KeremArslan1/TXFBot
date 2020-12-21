5603b9e9f928c7ef7d34f362948245946b756862
Kerem Arslan <75943060+KeremArslan1@users.noreply.github.com>
2020-12-21 21:11:18 +0300

    #1
    Bot is successfully working now

---
+++ lib/bot/__init__.py
@@ -0,0 +1,49 @@
+from discord import Intents
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord.ext.commands import Bot as BotBase
+
+PREFIX = "T-"
+OWNER_IDS = [385800441709068288]
+
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+
+        super().__init__(
+            command_prefix=PREFIX,
+            owner_ids=OWNER_IDS,
+            Intents=Intents.all(),
+            )
+
+    def run(self, version):
+        self.VERSION = version
+
+        with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
+
+        print("Bot çalıştırılıyor...")
+        super().run(self.TOKEN, reconnect =True)
+
+    async def on_connect():
+        print("Bot bağlandı!")
+
+    async def on_disconnect(self):
+        print("Bot'un bağlantısı koptu!")
+
+    async def on_ready(self):
+        if not self.ready:
+            self.ready = True
+            self.guild = self.get_guild(720314831818719253)
+            print("Bot hazır!")
+
+        else:
+            print("Bot yeniden bağlandı!")
+
+    async def on_message(self, message):
+        pass
+
+bot = Bot()
